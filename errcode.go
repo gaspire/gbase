ee7333fc5b4305fcd612a94e42b6b4ce9b385328
shizeng <zhongshiz@waypal.com>
2019-04-06 12:49:44 +0800

    fx

--- errcode.go
+++ errcode.go
@@ -2,10 +2,10 @@ package base
 
 // ErrCode 错误码
 var ErrCode = map[string]string{
-
 	"40400": "Not Found",
 
 	"50000": "Server error",
 	"50001": "Directory creation failed",
 	"50002": "File copy failed",
+	"50003": "This Email has submited",
 }
