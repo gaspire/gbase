538a2d33408a2eb4f3cb02e994300a3fe55c3ce4
shizeng <zhongshiz@waypal.com>
2019-05-09 16:16:13 +0800

    fx

--- config.go
+++ config.go
@@ -23,6 +23,10 @@ const (
 var (
 	//RateLimit API频率限制
 	RateLimit, _ = strconv.Atoi(os.Getenv("RateLimit"))
-	// ApplyFee 报名费用/单价
-	ApplyFee, _ = strconv.ParseFloat(os.Getenv("APPLY_FEE"), 64)
+	// SubmissionFee 摘要费用
+	SubmissionFee, _ = strconv.ParseFloat(os.Getenv("SUBMISSION_FEE"), 64)
+	// ApplyRegularFee 普通用户费用
+	ApplyRegularFee, _ = strconv.ParseFloat(os.Getenv("APPLY_REGULAR_FEE"), 64)
+	// ApplyStudentFee 学生费用
+	ApplyStudentFee, _ = strconv.ParseFloat(os.Getenv("APPLY_STUDENT_FEE"), 64)
 )
